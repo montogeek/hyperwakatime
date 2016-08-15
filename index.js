21b94d816fbb544d28c3da51ee85e8830b4e9f11
Fernando Montoya <montogeek@gmail.com>
2016-08-15 08:34:34 -0500

    Removed console.log

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 let xhr = require('xhr');
 
-let API_KEY = '';
 let API_URL = 'https://wakatime.com/api/v1/heartbeats';
 
-exports.onApp = app => {
-  API_KEY = app.config.getConfig().wakatimeApiKey;
-}
-
 exports.middleware = (state) => (next) => (action) => {
-  console.log(API_KEY);
-  
+  let apiKey = config.getConfig().wakatimeApiKey;
+
   const options = {
     time: Date.now()/1000,
     type: 'app',
@@ -23,9 +18,12 @@ exports.middleware = (state) => (next) => (action) => {
       method: 'POST',
       body: JSON.stringify(options),
       headers: {
-        'Authorization': `Basic ${btoa(API_KEY)}`
+        'Authorization': `Basic ${btoa(apiKey)}`
       }
     }, (err, response) => {
+      if(response.statusCode !== 201) {
+        console.error(response);
+      }
     })
   }
   next(action);
